ffa955d3a92b41148e918dff0eaf896baabbe406
betoleitao5 <betoleitao5@gmail.com>
2022-10-04 17:44:43 -0300

    Exemplo
    exemplo de tudo

---
+++ includes/_header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Hello, world!</title>
+  </head>
+
+  <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="./index.php">Index</a></li>
+                <li><a href="./noticia.php">Noticia</a></li>
+                <li><a href="./editoria.php">Editoria</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ noticia.php
@@ -0,0 +1,15 @@
+<?php
+//include de header
+include_once './includes/_header.php';
+// parte do conteudo da pagina
+?>
+<main>
+    <h1>noticia</h1>
+</main>
+<?php
+// include do aside
+include_once './includes/_aside.php';
+
+// include do footer
+include_once './includes/_footer.php';
+?>
