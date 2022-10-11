71e3839789397a95c89e3f4f76d414941cee990d
Roberto e Christian <betoleitao5@gmail.com>
2022-10-11 17:41:13 -0300

    V1.1

--- includes/_header.php
+++ includes/_header.php
@@ -1,30 +1,52 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <title>Hello, world!</title>
-  </head>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-  <body>
-    <header>
-      <img src="." alt="logo">
-        <nav>
-          <div>
-              <ul>
-                  <li><a href="./index.php">Menu</a></li>
-                  <li><a href="./noticia.php">Noticia</a></li>
-                  <li><a href="./editoria.php">Editoria</a></li>
-              </ul>
-              <div>
-                  <input type="search" name="pesquisa" id="pesquisa" placeholder="buscar">
-              </div>
-            </div>
-        </nav>
-    </header>
+  <link rel="stylesheet" href="./assets/css/header.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <header class="container-fluid">
+    <div class="logoc">
+    <img id="logoimg" src="https://pa1.narvii.com/6859/aa91a22ceae6628b069a9e40c5abc88e34bacc87_hq.gif" alt="logo">
+    </div>
+    <div class="row">
+      <nav class="navbar navbar-expand-lg navbar-light col-12">
+        <div class="col-12 col-lg-8">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-autu">
+              <li class="nav-item">
+                <a class="nav-link text-light" href="./index.php">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="./noticia.php">Noticia</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="./editoria.php">Editoria</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Poemas</a>
+              </li>
+          </div>
+        </div>
+        <div class="col-4">
+          <form class="form-inline my-2 my-lg-0 text-right">
+            <input class="form-control col-8 mr-0 mr-lg-2 bg btn-outline-white" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light col-3 my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  </header>
