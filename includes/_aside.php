aa3041216409a3a505298e17a3efd2bc38a9680d
Sari-Dorneles <sariadornels35@gmail.com>
2022-10-18 17:51:12 -0300

    Aside
    Aside em andamento

--- includes/_aside.php
+++ includes/_aside.php
@@ -1,3 +1,22 @@
 <aside>
+<section>
+<h4 class="A4">Lorem</h4>
+    <ul>
+       <a href=""><li>lorem ipsum</li></a>
+       <a href=""><li>lorem ipsum</li></a>
+       <a href=""><li>lorem ipsum</li></a>
+       <a href=""><li>lorem ipsum</li></a>
+       <a href=""><li>lorem ipsum</li></a>
+       <a href=""><li>lorem ipsum</li></a>
+
+    </ul>
+</section>
+<section>
+    <a href=""><img class="aimg" src="./contents/propaganda.jpg" alt=""></a>
+</section>
+<section id='wrap'>
+    <div id='calendar'></div>
+    <div style='clear:both'></div>
+</section>    
 
 </aside>
--- includes/_header.php
+++ includes/_header.php
@@ -7,8 +7,9 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="./assets/css/calendario.css">
     <link rel="stylesheet" href="./assets/css/style.css">
+    
     <title>Hello, world!</title>
   </head>
 
