5772fcd1f8ff185ca1ceb1e0a9ba088e3c94b90c
Framezinho <GabrielFrame25@gmail.com>
2022-10-19 15:15:44 -0300

    sem calendario mas completo o resto
    .

--- includes/_aside.php
+++ includes/_aside.php
@@ -1,6 +1,6 @@
 <aside>
 <section>
-<h4 class="A4">Lorem</h4>
+<h4 class="A4">Artigos de opiniões</h4>
     <ul>
        <a href=""><li>lorem ipsum</li></a>
        <a href=""><li>lorem ipsum</li></a>
@@ -12,8 +12,9 @@
     </ul>
 </section>
 <section>
-    <a href=""><img class="aimg" src="./contents/propaganda.jpg" alt=""></a>
+    <a target="_blank" href="https://www.senacrs.com.br/cursos/barbearia-classica-avancada_WyI2NTg1NiIsIjgzIixudWxsLCIxIl0?utm_source=googleads&utm_medium=cpc&utm_campaign=barbeariaclassica&utm_id=187978&gclid=EAIaIQobChMIvI6yndns-gIVDjORCh3rOgNKEAAYASAAEgK8wvD_BwE"><img class="aimg" src="./contents/propaganda.jpg" alt="Propaganda"></a>
 </section>
+<button type="button" class="btn btn-secondary btn-lg btn btn-dark">Calendario</button>
 <section>
    
 </section>    
