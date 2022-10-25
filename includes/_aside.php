dae4aa5e8e3473fa70a1a857a1f12e68cc426964
Luskete <>
2022-10-25 16:41:40 -0300

    Pronto pra juntar

--- includes/_aside.php
+++ includes/_aside.php
@@ -1,22 +1,22 @@
-<aside>
-<section>
-<h4 class="A4">Artigos de opiniões</h4>
-    <ul>
-       <a href=""><li>lorem ipsum</li></a>
-       <a href=""><li>lorem ipsum</li></a>
-       <a href=""><li>lorem ipsum</li></a>
-       <a href=""><li>lorem ipsum</li></a>
-       <a href=""><li>lorem ipsum</li></a>
-       <a href=""><li>lorem ipsum</li></a>
-
-    </ul>
-</section>
-<section>
-    <a target="_blank" href="https://www.senacrs.com.br/cursos/barbearia-classica-avancada_WyI2NTg1NiIsIjgzIixudWxsLCIxIl0?utm_source=googleads&utm_medium=cpc&utm_campaign=barbeariaclassica&utm_id=187978&gclid=EAIaIQobChMIvI6yndns-gIVDjORCh3rOgNKEAAYASAAEgK8wvD_BwE"><img class="aimg" src="./contents/propaganda.jpg" alt="Propaganda"></a>
-</section>
-<button type="button" class="btn btn-secondary btn-lg btn btn-dark">Calendario</button>
-<section>
-   
-</section>    
-
+<aside class="col-12 col-lg-4">
+    <div class="row">
+        <section class="col-12">
+            <h4 class="A4">Artigos de opiniões</h4>
+                <ul>
+                    <a href=""><li>lorem ipsum</li></a>
+                    <a href=""><li>lorem ipsum</li></a>
+                    <a href=""><li>lorem ipsum</li></a>
+                    <a href=""><li>lorem ipsum</li></a>
+                    <a href=""><li>lorem ipsum</li></a>
+                    <a href=""><li>lorem ipsum</li></a>
+                </ul>
+        </section>
+        <section class="col-12">
+            <a target="_blank" href="https://www.senacrs.com.br/cursos/barbearia-classica-avancada_WyI2NTg1NiIsIjgzIixudWxsLCIxIl0?utm_source=googleads&utm_medium=cpc&utm_campaign=barbeariaclassica&utm_id=187978&gclid=EAIaIQobChMIvI6yndns-gIVDjORCh3rOgNKEAAYASAAEgK8wvD_BwE"><img class="aimg" src="./contents/propaganda.jpg" alt="Propaganda"></a>
+        </section>
+            <button type="button" class="btn btn-secondary btn-lg btn btn-dark mx-auto">Calendário</button>
+        <section class="col-12">
+    
+        </section>  
+    </div>  
 </aside>
