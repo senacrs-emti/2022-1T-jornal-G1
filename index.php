a9c351ab2d650d0e5956207b793b51549320be17
betoleitao5 <betoleitao5@gmail.com>
2022-10-25 17:17:50 -0300

    update V4
    v4

--- index.php
+++ index.php
@@ -3,50 +3,9 @@
 include_once './includes/_header.php';
 // parte do conteudo da pagina
 ?>
-<main class="container-fluid col-8">
-    <div class="row">
-        <div>
-            <h1>Principais Notícias</h1>
-            <hr class="bg-dark">
-        </div>
-            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img src="https://picsum.photos/200/300" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Lorem Ipsum Dolor</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                </div>
-                <div class="carousel-item">
-                <img src="https://picsum.photos/200/300" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Lorem Ipsum Dolor</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                </div>
-                <div class="carousel-item">
-                <img src="https://picsum.photos/200" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Lorem Ipsum Dolor</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </button>
-        </div>
+<main class="container-fluid">
+    <div class="row col-8">
+        
     </div>
 </main>
 <?php
