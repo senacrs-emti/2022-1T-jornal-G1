462d49d82662e14adbaefccffa89847872f96e81
betoleitao5 <betoleitao5@gmail.com>
2022-10-26 15:36:32 -0300

    V6.1
    main 80%

--- includes/_footer.php
+++ includes/_footer.php
@@ -64,6 +64,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 <script src="./assets/js/calendario.js"></script>
 <script src="./assets/js/script.js"></script>
+<script src="./assets/js/loadmore.js"></script>
 
 
 </body>
