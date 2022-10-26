8ad3a59fdeb7fa4e8b7d8b1b751be0560b2064a6
CrisolKnt <sentientvoador@gmail.com>
2022-10-26 15:39:43 -0300

    V6.2
    70% Editoria

--- editoria.php
+++ editoria.php
@@ -4,19 +4,58 @@ include_once './includes/_header.php';
 // parte do conteudo da pagina
 ?>
 <link rel="stylesheet" href="editoria.css">
-<main>
+<main class="col-12 col-lg-8">
     <div class="row">
         <div class="col-8">
             <h1>editorial</h1>
         </div>
     </div>
 
-<div class="container">
-  <img src="https://classic.exame.com/wp-content/uploads/2022/08/urn_newsml_afp.com_20220816_872985e1-b0e6-43ea-8ebc-914fe1723af4_ipad.jpg?quality=70&strip=info&w=1024" alt="Snow" style="width:100%;">
-  <div class="centered"></div>
+    <div class="Editoria-M mb-5 col-12">
+        <div class="card h-100 w-100" style="background-image: url('https://n9e5v4d8.ssl.hwcdn.net/uploads/cecaf1b4c065c57e0d4e9033233044f9.jpg'); width: 200px; height: 100px; background-repeat: no-repeat;">
+            <span class="mx-auto">Texto</span>
+        </div>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-2">
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://n9e5v4d8.ssl.hwcdn.net/uploads/cecaf1b4c065c57e0d4e9033233044f9.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://n9e5v4d8.ssl.hwcdn.net/uploads/cecaf1b4c065c57e0d4e9033233044f9.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://n9e5v4d8.ssl.hwcdn.net/uploads/cecaf1b4c065c57e0d4e9033233044f9.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://n9e5v4d8.ssl.hwcdn.net/uploads/cecaf1b4c065c57e0d4e9033233044f9.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
 </div>
-
-
+</div>
 </main>
 <?php
 // include do aside
