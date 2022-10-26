688fa91498194df481f2d8acf793db35c052ed40
betoleitao5 <betoleitao5@gmail.com>
2022-10-26 13:59:34 -0300

    v6

--- editoria.php
+++ editoria.php
@@ -4,7 +4,7 @@ include_once './includes/_header.php';
 // parte do conteudo da pagina
 ?>
 <link rel="stylesheet" href="editoria.css">
-<main>
+<main class="container col-8">
     <div class="row">
         <div class="col-8">
             <h1>editorial</h1>
@@ -13,7 +13,7 @@ include_once './includes/_header.php';
 
 <div class="container">
   <img src="https://classic.exame.com/wp-content/uploads/2022/08/urn_newsml_afp.com_20220816_872985e1-b0e6-43ea-8ebc-914fe1723af4_ipad.jpg?quality=70&strip=info&w=1024" alt="Snow" style="width:100%;">
-  <div class="centered"></div>
+  <div class="centered"><p>papapapa</p></div>
 </div>
 
 
