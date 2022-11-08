efcf1d4d1ed2f68e9f2c881ea2e82a4faea20e40
Sari-Dorneles <sariadornels35@gmail.com>
2022-11-08 16:12:51 -0300

    Em andamento MAIN
    Em andamento...

--- noticia.php
+++ noticia.php
@@ -5,14 +5,14 @@ include_once './includes/_header.php';
 ?>
 <main class="container col-12 col-lg-8">
     <div class="row">
-        <h1 class="ml-4 mt-5 mb-2" style="color:white;">Lorem ipsum</h1>
-        <h5 class=" ml-4" style="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, sunt incidun</h5>
+        <h1 class="h1not" style="color:white;">Lorem ipsum</h1>
+        <h5 class="h5not" style="color: white;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, sunt incidun</h5>
         <img class="col-5 col-lg-9 ml-5 mt-4" src="./contents/imgmain.jpg" alt="imagem">
         <h5 class="text-justify  ml-5 mt-1" style="color: white;">Lorem ipsum dolor sit amet consectetur adipisicing elit. </h5>
         <p class="text-justify ml-5 mr-5" style="color: white;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, laboriosam quaerat voluptas tempora sunt totam deserunt enim accusantium dicta natus iusto eius iste aliquid praesentium ab ut nam voluptatem. Blanditiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed consequatur ipsa obcaecati nam quasi voluptates, vero dolores blanditiis culpa voluptas saepe voluptatum ratione quam ullam sapiente, doloribus, commodi totam. Perferendis!</p>
         <p class="text-justify ml-5 mr-5 " style="color: white;"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque magni doloribus assumenda ut explicabo illum cumque enim, error placeat, voluptate saepe laboriosam! Impedit cumque perspiciatis libero excepturi, veniam deleniti! Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci magni mollitia dolorum consequuntur libero, unde amet debitis ducimus commodi deserunt velit hic officia magnam necessitatibus. Alias maiores quos excepturi libero?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, reprehenderit recusandae! Quia ex, inventore voluptatum magnam, ea tempora esse accusantium suscipit, dicta corrupti ullam quidem tempore distinctio reiciendis facere veritatis.</p>
         <div class="row row-cols-1 row-cols-md-2">
-                <h2 class="ml-5">NOTÍCIAS SEMELHANTES</h2>
+                <h2 class="h2not">NOTÍCIAS SEMELHANTES</h2>
                 <hr class="col-12 bg-light">
             <div class="col mb-4">
                 <div class="card bg-transparent border-0">
