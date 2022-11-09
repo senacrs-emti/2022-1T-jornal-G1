4c0459a471391b4d00765d029ab9eae40474e426
CrisolKnt <sentientvoador@gmail.com>
2022-11-09 14:59:31 -0300

    Supostamente atualização da 3º Pasta

---
+++ 1_Artigo/1_Artigo_Samu.php
@@ -0,0 +1,65 @@
+<?php
+//include de header
+include_once './includes/_header.php';
+// parte do conteudo da pagina
+?>
+<main class="container col-12 col-lg-8">
+    <div class="row col-12">
+        <h1 class="h1not" >Lorem ipsum</h1>
+        <h5 class="h5not" >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, sunt incidun</h5>
+        <img class="imgum" src="./contents/imgmain.jpg" alt="imagem">
+        <div class="textoimagem"><h4 class="textolegal text-justify" style="color: white;">Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4></div>
+        <p class="pnoticia text-justify" style="color: white;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, laboriosam quaerat voluptas tempora sunt totam deserunt enim accusantium dicta natus iusto eius iste aliquid praesentium ab ut nam voluptatem. Blanditiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed consequatur ipsa obcaecati nam quasi voluptates, vero dolores blanditiis culpa voluptas saepe voluptatum ratione quam ullam sapiente, doloribus, commodi totam. Perferendis!</p>
+        <p class="pnoticia text-justify" style="color: white;"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque magni doloribus assumenda ut explicabo illum cumque enim, error placeat, voluptate saepe laboriosam! Impedit cumque perspiciatis libero excepturi, veniam deleniti! Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci magni mollitia dolorum consequuntur libero, unde amet debitis ducimus commodi deserunt velit hic officia magnam necessitatibus. Alias maiores quos excepturi libero?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, reprehenderit recusandae! Quia ex, inventore voluptatum magnam, ea tempora esse accusantium suscipit, dicta corrupti ullam quidem tempore distinctio reiciendis facere veritatis.</p>
+        <div class="row row-cols-1 row-cols-md-2">
+                <h2 class="h2not">NOTÍCIAS SEMELHANTES</h2>
+                <hr class="col-12 bg-light">
+            <div class="col mb-4">
+                <div class="card bg-transparent border-0">
+                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4" style="height: 100px; width: 350px;"    alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:white;">Card title</h5>
+
+                    </div>
+                </div >
+            </div>
+            <div class="col mb-4">
+                <div class="card  bg-transparent border-0">
+                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:white;">Card title</h5>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card  bg-transparent border-0">
+                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:white;">Card title</h5>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card  bg-transparent border-0">
+                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:white;">Card title</h5>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</main>
+<?php
+// include do aside
+include_once './includes/_aside.php';
+
+// include do footer
+include_once './includes/_footer.php';
+?>
