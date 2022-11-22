33ab5b1e8d9378d876d7fabbe4508771f0556c0b
Jujubileuu <augustusvalini@gmail.com>
2022-11-22 15:22:17 -0300

    Formatação Lollapalooza
    Formatação do texto.

--- Noticia_Ingressos_lollapalooza.php
+++ Noticia_Ingressos_lollapalooza.php
@@ -5,14 +5,17 @@ include_once './includes/_header.php';
 ?>
 <main class="container col-12 col-lg-8">
     <div class="row col-12">
-        <h1 class="h1not" >Venda dos ingressos Lollapalooza 2023</h1>
-        <h5 class="h5not" >Está disponível a compra dos ingressos para o evento que ocorrerá no ano que vem</h5>
+        <h1 class="h1not font-weight-bold" >Venda dos ingressos Lollapalooza 2023</h1>
+        <h5 class="h5not mb-5" >Está disponível a compra dos ingressos para o evento que ocorrerá no ano que vem</h5>
+        <p class="text-light pnoticia"><strong>RAUL RODRIGUES</strong> Cursando</p>
+        <b class="text-muted pnoticia">22/11/2022 - 14:30</b>
+        <hr class="bg-light col-10 mt-1">
         <img class="imgum" src="./contents/lollapalooza-2023.jpg" alt="lollapalooza">
         <div class="textoimagem"><h4 class="textolegal text-justify" style="color: white;">Lollapalooza 2023 </h4></div>
         <p class="pnoticia text-justify" style="color: white;">Saíram os ingressos para o maior festival de São Paulo, o Lollapalooza. O evento acontecerá nos dias 24, 25 e 26 de março de 2023 no Autódromo de Interlagos e contará com muitos artistas incríveis e muito populares atualmente, com músicas marcantes e que muitas vezes são consideradas hits.</p>
         <p class="pnoticia text-justify" style="color: white;">Os ingressos para um dia custam de 600 a 700 reais, variando do dia que será escolhido pelo comprador para comprar os ingressos, já para ter ingresso para os shows do dia 24 a 26 de março, ou seja, o início e fim do evento, os ingressos podem variar os preços entre os valores de 1.500 reais e 3.600 reais, variando  baseado no ingresso que a pessoa comprará, dependendo se é estudante ou em qual banco que utilizará para efetuar a compra, já que em alguns casos pode se ter descontos inclusos por outros benefícios voltados para pessoas deficientes. </p>
-        <img class="imgum" src="./contents/lollashows.jpg" alt="lollapalooza shows">
-        <div class="textoimagem"><h4 class="textolegal text-justify" style="color: white;">lollapalooza shows </h4></div>
+        <img class="imgum mt-3" src="./contents/lollashows.jpg" alt="lollapalooza shows">
+        <div class="textoimagem"><h4 class="textolegal text-justify" style="color: white;">Lollapalooza shows </h4></div>
         <p class="pnoticia text-justify" style="color: white;">E na parte dos shows podemos perceber que a lista está enorme, com muitos artistas que são fenômenos hoje em dia como Billie Eilish, Lil Nas X, Rosália, Blink-182, Tame Impala, Cigarettes after sex, Drake e muitos outros. Os shows prometem muito e estão todos ansiosos para este evento, já que, como mencionado anteriormente, é um dos maiores eventos de São Paulo e desta vez promete muito a ter os melhores shows. </p>
         <div class="row mt-5">
                 <h2 class="h2not">NOTÍCIAS SEMELHANTES</h2>
