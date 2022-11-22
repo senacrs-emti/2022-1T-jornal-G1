522f37dad34666c1ad7ab8ea5313bb2fbec406c1
betoleitao5 <betoleitao5@gmail.com>
2022-11-22 15:00:08 -0300

    V7.7
    +Noticia album da copa 2022
    +fixed bugs

---
+++ Noticia_Album_copa_2022.php
@@ -0,0 +1,95 @@
+<?php
+//include de header
+include_once './includes/_header.php';
+// parte do conteudo da pagina
+?>
+<main class="container col-12 col-lg-8">
+    <div class="row col-12">
+        <h1 class="h1not" >ÁLBUM DA COPA DE 2022 E SEU ENORME SUCESSO</h1>
+        <h5 class="h5not">O novo álbum da copa de 2022 virou febre entre jovens, adultos e colecionadores do mundo todo com várias novas novidades.</h5>
+        <p class="pnoticia text-justify" style="color: white;">A nova febre que percorre pelo mundo todo é o álbum da copa de 2022, lançado no dia 19 de agosto. Sendo distribuído pela Panini em 14 países da américa latina sendo um deles o Brasil, fazendo sucesso não apenas entre os mais jovens e sim até entre adultos, colecionadores e pessoas da terceira idade.</p>
+        <img class="imgum" src="./contents/album2022.jpg" alt="">
+        <div class="textoimagem"><h4 class="textolegal text-justify" style="color: white;">Album da copa sendo vendida pela marca panini </h4></div>
+        <p class="pnoticia text-justify" style="color: white;">	Com 670 figurinhas para completar o álbum, sendo 540 comum, 80 raras e 50 especiais sendo assim preciso 134 pacotes para adquirir as 670 figurinhas no valor de R$ 536,00, vale relembrar que isso é no caso de não vier nenhuma figurinha repetida. O valor atual de cada pacote é de R$ 4,00, sendo o quádruplo do preço da copa de 2018, na época os pacotes eram vendidos por R$ 1,00.</p>
+        <p class="pnoticia text-justify" style="color: white;"> Uma novidade do álbum da copa de 2022 são as novas figurinhas legends, são uma série de 20 figurinhas consideradas extras do conjunto de 670 do álbum, elas são divididas em 14 jogadores lendas e 6 novatos promissores e tem suas versões alternativas que são bordô, bronze, prata e ouro totalizando 80 figurinhas legends.</p>
+        <h5 class="pnoticia text-justify" style="color: white;" >Abaixo estão listados os nomes dos jogadores que possuem sua versão legend:</h5>
+        
+        <ul class="pnoticia text-justify" style="color: white;">
+            <li>Giovanni Reyna (EUA)</li>
+            <li>Luis Suárez (Uruguai)</li>
+            <li>Kevin de Bruyne (Bélgica)</li>
+            <li>Robert Lewandowski (Polônia)</li>
+            <li>Almoez Ali (Catar)</li>
+            <li>Gavi (Espanha)</li>
+            <li>Heung-Min Son (Coreia do Sul)</li>
+            <li>Luka Modric (Croácia)</li>
+            <li>Raphaël Varane (França)</li>
+            <li>Sadio Mané (Senegal)</li>
+            <li>Alphonso Davies (Canadá)</li>
+            <li>Ryan Gravenberch (Holanda)</li>
+            <li>Neymar Jr (Brasil)</li>
+            <li>Kylian Mbappé (França)</li>
+            <li>Lionel Messi (Argentina)</li>
+            <li>Dusan Vlahovic (Sérvia)</li>
+            <li>Jude Bellingham (Inglaterra)</li>
+            <li>Cristiano Ronaldo (Portugal)</li>
+            <li>Christian Eriksen (Dinamarca)</li>
+            <li>Guillermo Ochoa (México)</li>
+        </ul>
+
+        <p class="pnoticia text-justify" style="color: white;">Algumas dessas 20 figurinhas extras como o Neymar de ouro podem ser encontradas na internet, sendo vendidas em torno de R$ 7 mil a R$ 10 mil reais, alguns colecionadores fizeram uma pesquisa onde é preciso em torno de 1800 pacotes que tirar uma figurinha legend. Uma tática usada pelas pessoas para conseguir mais rápido as legend é pesar os pacotes, porque elas pesam mais que figurinhas normais devido aos detalhes e materiais utilizados.</p>
+        <p class="pnoticia text-justify" style="color: white;">Uma página especial do álbum é com patrocínio da Coca-Cola, que começou a circular no dia 19 de setembro dentro do rótulo de garrafas da Coca-Cola zero de 1,5 litros a 2 litros, a página contém espaço para 8 figurinhas especiais de jogadores.</p>
+        <p class="pnoticia text-justify" style="color: white;">Para você que está cheio de figurinhas repetidas existem vários pontos de encontros em praças, shoppings, parques, bares entre outros lugares onde. E você que ainda não entrou na onda e está com medo de estar sem tempo, não se preocupe pois o álbum deve circular até o fim da copa que está prevista para começar dia 20 de novembro e terminar 18 de dezembro.</p>
+            
+            <div class="row mt-5">
+                <h2 class="h2not">NOTÍCIAS SEMELHANTES</h2>
+                <hr class="col-12 bg-light">
+            <div class="col mb-4">
+                <div class="card bg-transparent border-0">
+                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4" style="height: 100px; width: 350px;"    alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:white;">Card title</h5>
+
+                    </div>
+                </div >
+            </div>
+            <div class="col mb-4">
+                <div class="card  bg-transparent border-0">
+                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:white;">Card title</h5>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card  bg-transparent border-0">
+                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:white;">Card title</h5>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card  bg-transparent border-0">
+                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:white;">Card title</h5>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</main>
+<?php
+// include do aside
+include_once './includes/_aside.php';
+
+// include do footer
+include_once './includes/_footer.php';
+?>
