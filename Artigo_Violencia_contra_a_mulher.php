3c41331dec5343ad925437d462f6a35c9e547ebc
Jujubileuu <augustusvalini@gmail.com>
2022-11-29 15:43:54 -0300

    Artigos V2.1
    Artigo violência contra mulher arrumado.

--- Artigo_Violencia_contra_a_mulher.php
+++ Artigo_Violencia_contra_a_mulher.php
@@ -3,58 +3,6 @@
 include_once './includes/_header.php';
 // parte do conteudo da pagina
 ?>
-<main class="container col-12 col-lg-8">
-    <div class="row col-12">
-        <h1 class="h1not" >Artigo de opinião – Violência contra a mulher </h1>
-        <p class="pnoticia text-justify" style="color: white;">Atualmente, a violência contra a mulher é, infelizmente, uma realidade que revolta e entristece a população atual. Dia 08 de março não é apenas qualquer dia no calendário, é o Dia Internacional das Mulheres, é o dia em que milhares de mulheres não se sentem mais caladas, se sentem livres para expressar todas as injustiças, desigualdades e violência, sendo essas violências psicológicas, físicas ou sexuais. Nessa luta são colocados avanços para disseminar esses abusos. A violência contra a mulher segue padrões complexos, geralmente o agressor é próximo a vítima. Grande parte das mulheres defende o seu agressor, e alguns motivos são: medo de se expor, perdão, questão financeira, negação (não querendo aceitar que está sendo agredida por alguém pelo qual apresentava ser confiável), entre outros diversos fatoresA. A violência abrange todas nós, em qualquer lugar e momento, mulheres negras, idosas, crianças, adolescentes, lésbicas, trans e entre tantas outras que estamos vulneráveis; muitas pessoas não veem as agressões psicológicas como uma das formas de violência; a violência psicológica causa feridas emocionais profundas nas vítimas, demorando anos para cicatrizar.</p>
-        <p class="pnoticia text-justify" style="color: white;">As taxas de violência e feminicídio são gritantes, no primeiro semestre de 2022, o central de atendimentos registrou 31.398 denúncias e 169.676 violações envolvendo a violência doméstica contra as mulheres. Casos de feminicídio em 2022 superam em 60% todo o ano de 2021. A violência contra a mulher tem alta relevância na questão de saúde pública, uma vez que mulheres que vivem ou viveram em uma situação complicada têm mais queixas, distúrbios e patologias físicas e mentais; elas usufruem com mais frequência do serviço de saúde que aquelas que não vivenciaram a experiência. Estudos relatam consequências psicológicas e comportamentais da violência: uso de álcool, drogas, depressão, ansiedade, tabagismo, comportamentos suicidas e de autoflagelo, distúrbios na alimentação e no sono, baixa autoestima, fobias e síndrome do pânico.</p>
-        <p class="pnoticia text-justify" style="color: white;">É importante que a sociedade entenda que esse tipo de crime não pode ser tolerado, é necessário que as pessoas compreendam a gravidade do problema, pois precisamos do poder do público para o combate à violência, reforçando e construindo cada vez mais projetos, como casas de acolhimento, serviços de proteções reunindo dados que mostrem a raiz da violência para permitir intervenções diretas nessas causas. Necessitamos apoiar as mulheres que sofrem ou já sofreram qualquer tipo de violência, para que elas não se sintam sozinhas, precisamos dar voz para essas pessoas.</p>
-        
-        <div class="row mt-5">
-                <h2 class="h2not">NOTÍCIAS SEMELHANTES</h2>
-                <hr class="col-12 bg-light">
-            <div class="col mb-4">
-                <div class="card bg-transparent border-0">
-                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4" style="height: 100px; width: 350px;"    alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title" style="color:white;">Card title</h5>
-
-                    </div>
-                </div >
-            </div>
-            <div class="col mb-4">
-                <div class="card  bg-transparent border-0">
-                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title" style="color:white;">Card title</h5>
-
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-4">
-                <div class="card  bg-transparent border-0">
-                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title" style="color:white;">Card title</h5>
-
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-4">
-                <div class="card  bg-transparent border-0">
-                    <img src="./contents/imgcinza.jpg" class="card-img-top ml-4"  style="height: 100px; width: 350px;" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title" style="color:white;">Card title</h5>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
-</main>
 
 <main class="container col-12 col-lg-8 text-light">
     <div class="row col-12">
