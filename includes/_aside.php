2b857698fb14ca5f60e6a4a858b2e33c9fa39828
HallsRaul <109175578+HallsRaul@users.noreply.github.com>
2022-11-29 14:02:37 -0300

    Alterações
    Alteração na foto da home e no aside

--- includes/_aside.php
+++ includes/_aside.php
@@ -5,28 +5,28 @@
                     <h4 class="A4">Artigos de opiniões</h4>
                     <ul class="col-12">
                         <a class="text-light" href="./Artigo_jogos_indies.php">
-                            <li>Jogos indies</li>
+                            <li class="row mt-5 mb-5">Jogos indies</li>
                         </a>
                         <a class="text-light" href="./Artigo_RockandRio_2022.php">
-                            <li>Rock and Rio 2022</li>
+                            <li class="row mt-5 mb-5">Rock and Rio 2022</li>
                         </a>
                         <a class="text-light" href="./Artigo_Cassinos_Online.php">
-                            <li>Cassinos Online</li>
+                            <li class="row mt-5 mb-5">Cassinos Online</li>
                         </a>
                         <a class="text-light" href="./Artigo_jogos_atualmente.php">
-                            <li>Jogos Atualmente</li>
+                            <li class="row mt-5 mb-5">Jogos Atualmente</li>
                         </a>
                         <a class="text-light" href="">
-                            <li>lorem ipsum</li>
+                            <li class="row mt-5 mb-5">lorem ipsum</li>
                         </a>
                         <a class="text-light" href="">
-                            <li>lorem ipsum</li>
+                            <li class="row mt-5 mb-5">lorem ipsum</li>
                         </a>
                         <a class="text-light" href="">
-                        <li>Escute nossa entrevista em formato de podcast</li>
+                        <li class="row mt-5 mb-5">Escute nossa entrevista</li>
                         </a>
                         <a class="text-light" href="">
-                        <li>Leia nossa mais recente entrevista</li>
+                        <li class="row mt-5">Leia nossa mais recente entrevista</li>
                         </a>
                     </ul> 
                 </section>
