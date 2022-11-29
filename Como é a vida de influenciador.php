3a5c2cc3436e48856d9968d2cea1c686d083c7bd
HallsRaul <109175578+HallsRaul@users.noreply.github.com>
2022-11-29 17:32:48 -0300

    Entrevista Raul
    Falta a imagem do Christian

---
+++ Como é a vida de influenciador.php
@@ -0,0 +1,113 @@
+<?php
+//include de header
+include_once './includes/_header.php';
+// parte do conteudo da pagina
+?>
+
+<main class="container col-12 col-lg-8 text-light">
+    <div class="row col-12">
+        <div class="col-12">
+            <h1 class="font-weight-bold mt-4 titulo">Como é a vida de influenciador?</h1>
+            <h6 class="font-weight-normal subtitulo">Nessa entrevista o influenciador e youtuber Chris fala sobre aspectos da sua profissão.</h6>
+            <div class="pt-4">
+                <p class="text-light float-left"><strong>RAUL RODRIGUES ALMEIDA</strong> Estudante</p>
+                <b class="text-muted float-right">29/11/2022 - 14:36</b>
+            </div>
+            <hr class="bg-light mt-4">
+            <div class="paragrafo-artigo">
+                <p>Nesta sexta-feira, dia 18 de novembro, nossa equipe de entrevistadores conversou com Chris, ou Christian, um influenciador digital do Instagram, que fala sobre conteúdos voltados a cultura geek e nerd, com assuntos diversos, desde filmes sobre cinema, música, animes, ciência e muito mais outros assuntos. O influenciador tem cerca de dois mil seguidores e possuí um canal com um milhão de inscritos no YouTube. </p>
+            </div>
+            <div class="imagem-artigo">
+                <img src="https://picsum.photos/1000/500" alt="..." class="w-100 mb-2 mt-2 imgLolla1">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, possimus sit excepturi culpa impedit.</p>
+            <div class="paragrafo-artigo">
+                <p>Nossa equipe quis fazer perguntas relacionadas a como ele acha que a internet pode influenciar escolhas, visões de mundo e sobre como pode ajudar as pessoas, sendo a primeira pergunta: como a internet pode influenciar nas pessoas? .</p>
+            </div>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>- A internet é uma coisa incrível, pode espalhar informação e conhecimento instantaneamente, e pode adicionar muito na vida das pessoas, com conteúdo de grande importância culturalmente, historicamente e cientificamente para o mundo, seja antigamente ou até nos dias atuais, mas a internet pode se tornar também algo muito perigoso se usada de forma errada, com riscos de as pessoas sofrerem com influências negativas que podem ter consequências, mas isso tudo vem de acordo com como e com que nível de responsabilidade é utilizada essa ferramenta. </p>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>Em seguida os entrevistadores decidiram perguntar qual é a função de um influenciador digital:</p>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>- Um influenciador tem um papel muitas vezes muito importante, já que em muitos casos ele estimula as pessoas a adotarem um costume, comportamento, mudam a visão de mundo e das coisas para a pessoa muitas vezes, a estimulam a comprar coisas, a começar algo na sua vida, as colocam para cima, sendo carismáticos e gerando um lado mais amigo de ser e trazendo essas pessoas para seu lado e das marcas que te apoiam e patrocinam, gerando uma base de fãs. </p>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>Com essa resposta a próxima pergunta foi sobre a vida de um influenciador:</p>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>- A vida de um influenciador depende muito com questões de rotina, já que são dados trabalhos que podem vir do próprio criador de conteúdo quanto de marcas e empresas de fora que podem vir a te contratar ou chamar para fazer um trabalho ou para falar e mostrar mais sobre a marca deles, mas é uma rotina de muito trabalho e de muita atenção e estudo sobre o dia a dia e estar ligado nas coisas que estão acontecendo no meio que você se baseia para criar conteúdo. </p>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>Então a equipe partiu para uma pergunta um pouco mais pessoal, em que o entrevistado poderia responder de acordo com sua preferência, a pergunta era: como é a sua vida sendo influencer? </p>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>- Minha vida é bem movimentada, com muitas viagens para eventos e encontros com outras pessoas para criar conteúdo, além disso faço muitos vídeos e stories sobre muitos conteúdos e marcas diferentes, eu gosto da minha vida mesmo que seja bem movimentada pois eu consigo aproveitar e conhecer muitos lugares e coisas sobre o mundo e as coisas que eu gosto. </p>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>E para finalizar, qual é a dica que você deixa para nossos leitores que pretendem se tornar influenciadores?</p>
+            </div>
+            <div class="paragrafo-artigo">
+                <p>- A minha dica é simples, pois assim como para qualquer coisa que você vá fazer na sua vida, você precisa de dedicação, precisa tentar, estudar, ler sobre e ir tentando até conseguir seu objetivo, a vida é feita de erros e erros até o momento em que você acerta o objetivo, então não desista e persista no seu sonho, pois assim você conseguirá criar sua própria história e ser feliz com o que conseguiu. </p>
+            </div>
+            <section class="semelhantes">
+                <h3 class="font-weight-bold mt-5">NOTÍCIAS SEMELHANTES</h3>
+                <hr class="bg-light">
+                <div class="card mb-3 bg-transparent border-bottom">
+                    <div class="row no-gutters">
+                        <div class="col-3 col-lg-2">
+                            <img src="contents/album2022.jpg" alt="album2022" class="w-60" style="height: 120px;">
+                        </div>
+                        <div class="col-9 col-lg-10">
+                            <div class="card-body ml-4 mt-1">
+                                <a href="" class="text-light">
+                                    <h5 class="card-title font-weight-bold h5-outras">ÁLBUM DA COPA DE 2022 E SEU ENORME SUCESSO</h5>
+                                    <p class="card-text p-outras">O novo álbum da copa de 2022 virou febre entre jovens, adultos e colecionadores do mundo todo com várias novas novidades.</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3 bg-transparent border-bottom">
+                    <div class="row no-gutters">
+                        <div class="col-3 col-lg-2">
+                            <img src="contents/album2022.jpg" alt="album2022" class="w-60" style="height: 120px;">
+                        </div>
+                        <div class="col-9 col-lg-10">
+                            <div class="card-body ml-4 mt-1">
+                                <a href="" class="text-light">
+                                    <h5 class="card-title font-weight-bold h5-outras">ÁLBUM DA COPA DE 2022 E SEU ENORME SUCESSO</h5>
+                                    <p class="card-text p-outras">O novo álbum da copa de 2022 virou febre entre jovens, adultos e colecionadores do mundo todo com várias novas novidades.</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3 bg-transparent border-bottom">
+                    <div class="row no-gutters">
+                        <div class="col-3 col-lg-2">
+                            <img src="contents/album2022.jpg" alt="album2022" class="w-60" style="height: 120px;">
+                        </div>
+                        <div class="col-9 col-lg-10">
+                            <div class="card-body ml-4 mt-1">
+                                <a href="" class="text-light">
+                                    <h5 class="card-title font-weight-bold h5-outras">ÁLBUM DA COPA DE 2022 E SEU ENORME SUCESSO</h5>
+                                    <p class="card-text p-outras">O novo álbum da copa de 2022 virou febre entre jovens, adultos e colecionadores do mundo todo com várias novas novidades.</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</main>
+
+<?php
+// include do aside
+include_once './includes/_aside.php';
+
+// include do footer
+include_once './includes/_footer.php';
+?>
