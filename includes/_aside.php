34f9df75f5930741c789ffbbca7de193cae44db9
CrisolKnt <108826660+CrisolKnt@users.noreply.github.com>
2022-11-29 15:00:53 -0300

    Margin nos textos do Aside

--- includes/_aside.php
+++ includes/_aside.php
@@ -5,29 +5,29 @@
                     <h4 class="A4">Artigos de opiniões</h4>
                     <ul class="col-12">
                         <a class="text-light" href="./Artigo_jogos_indies.php">
-                            <li class="row">Jogos indies</li>
+                            <li class="row pl-3">Jogos indies</li>
                         </a>
                         <a class="text-light" href="./Artigo_RockandRio_2022.php">
-                            <li class="row ">Rock and Rio 2022</li>
+                            <li class="row pl-3">Rock and Rio 2022</li>
                         </a>
                         <a class="text-light" href="./Artigo_Cassinos_Online.php">
-                            <li class="row ">Cassinos Online</li>
+                            <li class="row pl-3">Cassinos Online</li>
                         </a>
                         <a class="text-light" href="./Artigo_jogos_atualmente.php">
-                            <li class="row ">Jogos Atualmente</li>
+                            <li class="row pl-3">Jogos Atualmente</li>
                         </a>
                         <a class="text-light" href="">
-                            <li class="row ">lorem ipsum</li>
+                            <li class="row pl-3">lorem ipsum</li>
                         </a>
                         <a class="text-light" href="">
-                            <li class="row">lorem ipsum</li>
+                            <li class="row pl-3">lorem ipsum</li>
                         </a>
                         <h4 class="row A4_5">Entrevista</h4>
                         <a class="text-light" href="">
-                        <li class="row ">Escute nossa entrevista</li>
+                        <li class="row pl-3">Escute nossa entrevista</li>
                         </a>
                         <a class="text-light" href="">
-                        <li class="row ">Leia nossa mais recente entrevista</li>
+                        <li class="row pl-3">Leia nossa mais recente entrevista</li>
                         </a>
                     </ul> 
                 </section>
