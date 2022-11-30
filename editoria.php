cef52be5b67a4861b66b3897d86781be291e188b
Jujubileuu <augustusvalini@gmail.com>
2022-11-30 15:15:44 -0300

    Rename
    Rename nos códigos.

--- editoria.php
+++ editoria.php
@@ -3,14 +3,6 @@
 include_once './includes/_header.php';
 // parte do conteudo da pagina
 ?>
-<link rel="stylesheet" href="editoria.css">
-<div>
-    <main class="container col-12 col-lg-8 size ">
-
-
-    </div>
-    </main>
-
 <?php
 // include do aside
 include_once './includes/_aside.php';
